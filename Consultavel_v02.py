a82a087b396e540ff8a58e9b561f6838ea4c1ae1
IKFGMS <91566884+SnowingNewt@users.noreply.github.com>
2023-04-28 04:26:50 -0300

    Add files via upload

---
+++ Consultavel_v02.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from colorama import init, Fore, Style
+
+# Inicializa a biblioteca colorama
+init()
+
+# Define a função para imprimir a arte ASCII colorida
+def print_ascii_art():
+    print(Fore.RED + Style.BRIGHT + """
+
+██╗   ██╗███╗   ██╗██████╗ ███████╗██████╗     ██╗     ███████╗ █████╗ ██╗  ██╗███████╗
+██║   ██║████╗  ██║██╔══██╗██╔════╝██╔══██╗    ██║     ██╔════╝██╔══██╗██║ ██╔╝██╔════╝
+██║   ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝    ██║     █████╗  ███████║█████╔╝ ███████╗
+██║   ██║██║╚██╗██║██║  ██║██╔══╝  ██╔══██╗    ██║     ██╔══╝  ██╔══██║██╔═██╗ ╚════██║
+╚██████╔╝██║ ╚████║██████╔╝███████╗██║  ██║    ███████╗███████╗██║  ██║██║  ██╗███████║
+ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝    ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+
+"""+ Fore.GREEN + "Codado por: David Mascaro ;)" + Style.RESET_ALL)
+
+# Exibe a arte ASCII
+print_ascii_art()
+
+# Solicita o endereço de e-mail a ser consultado
+email = input("AH MALANDRO, QUER CONSULTAR UNS LEAKS? DIGITE O ENDEREÇO DE EMAIL: ")
+
+# Faz a consulta na API
+url = f"https://beta.snusbase.com/v2/combo/{email}"
+response = requests.get(url)
+
+# Verifica se a consulta foi bem sucedida
+if response.status_code == 200:
+    # Exibe os resultados da consulta
+    results = response.json()
+    print(f"Foram encontrados {len(results)} resultados para o e-mail {email}:")
+    print(json.dumps(results, indent=4, sort_keys=True))
+else:
+    # Exibe uma mensagem de erro
+    print(Fore.RED + f"Não foi possível consultar o e-mail {email}. Código de status: {response.status_code}" + Style.RESET_ALL)
